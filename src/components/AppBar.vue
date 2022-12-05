201eed7eb36bb53bdbeab518f5a8a1ad60d569b4
root <root@LAPTOP-LMJQULD0.localdomain>
2022-12-05 17:22:24 +0000

    As seen in class

---
+++ src/components/AppBar.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto overflow-hidden"
+    >
+      <v-app-bar
+        color="green accent-4"
+        dense
+        dark
+      >
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>My Cool App</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-airplane-alert</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              @click="() => {}"
+            >
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-help-circle</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-footer
+    v-bind="localAttrs"
+    :padless="padless"
+  >
+    <v-card
+      flat
+      tile
+      width="100%"
+      class="green lighten-1 text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="(icon, idx) in icons"
+          :key="idx"
+          class="mx-4"
+          icon
+        >
+        <router-link :to="{name: links[idx]}" >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </router-link>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>FULP</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar',
+  data () {
+    return {
+      icons: [
+        'mdi-home',
+        'mdi-email',
+        'mdi-calendar',
+        'mdi-account-cowboy-hat'
+      ],
+      links: [
+        'home',
+        'mail',
+        'calendar',
+        'cowboy'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
